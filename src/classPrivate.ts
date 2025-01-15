aa1041087a4c950992510072a29aba6db6c9c629
danilobarros3 <danilo.lionsoft@gmail.com>
2025-01-15 09:23:37 -0300

    :sparkles: feat: adding class private

--- src/classPrivate.ts
+++ src/classPrivate.ts
@@ -1,29 +1,65 @@
 import { log } from "./colors";
 
+//# É privado
 class GitUser {
-  _userName = "";
-  _repos = "";
+  #userName: string;
+  #repos: number;
+
+  constructor(userName: string, repos: number) {
+    this.#userName = userName;
+    this.#repos = repos;
+  }
 
   greeting() {
-    return `User ${this._userName}, have ${this._repos} repositories.`;
+    return `User ${this.#userName}, have ${this.#repos} repositories.`;
   }
 
   get userName() {
-    return this._userName;
+    return this.#userName;
   }
   set userName(name) {
-    this._userName = name;
+    this.#userName = name;
   }
 
   set repos(repos) {
-    this._repos = repos;
+    this.#repos = repos;
   }
 
   get repos() {
-    return this._repos;
+    return this.#repos;
   }
 }
 
-const gitUser = new GitUser();
+const gitUser = new GitUser("danilobarros3", 10);
+gitUser.userName = "John Doe";
+const nameArray = gitUser.userName.split(" ");
+console.log(nameArray)
+gitUser.repos = 10;
 
 log(gitUser.greeting());
+
+// const GitUsers = () => {
+//   let userName = "";
+//   let repos = 0;
+//   return {
+//     getUserName() {
+//       return userName;
+//     },
+//     setUserName(name: string) {
+//       return (userName = name);
+//     },
+//     getRepos() {
+//       return repos;
+//     },
+//     setRepos(rep: number) {
+//       return (repos = rep);
+//     },
+//     greeting() {
+//       return `User ${userName}, have ${repos} repositories.`;
+//     },
+//   };
+// };
+
+// const otherGithubUser = GitUsers()
+// otherGithubUser.setUserName("Jhon doe")
+// otherGithubUser.setRepos(15)
